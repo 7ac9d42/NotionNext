ada7b9f85f3e7c2517944e6e4c6b9098e97acb4a
KUAPT <110989625+KUAPT@users.noreply.github.com>
2024-05-22 21:29:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'KUAPT', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个爱摸鱼🐟的宅', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.kuapt.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'KUAPT','博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'KUAPT,博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'yanruiad@126.com', // 邮箱地址 例如mail@tangly1024.com
